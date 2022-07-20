8661da2460abd88b454ae452856870652ee88c30
Shubham Sati <shubham.sati8b@gmail.com>
2022-07-20 18:09:57 +0530

    HomePage Navbar commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Homepage from "./Elements/Pages/HomePage/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Shubham Sati</p>
-        <p>Priyanka sati</p>
-        <p>this is for checking</p>
-      </header>
-    </div>
+    <Homepage />
   );
 }
 
